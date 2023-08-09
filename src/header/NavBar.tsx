ffb2957baaeecb723c28e63f681632ed7be37b8a
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2023-08-09 15:58:29 +0100

    improves nav elements

--- src/header/NavBar.tsx
+++ src/header/NavBar.tsx
@@ -1,26 +1,47 @@
+import { Button, Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 
 function NavBar() {
   return (
-    <Nav
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <>
+      <div>
+        <div id='logo'>
+          <div>logo</div>
+          <div>mic</div>
+          <div>Maternity intensive care</div>
+        </div>
+        <Nav
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+          <Nav.Item>
+            <Nav.Link href="/home">About us</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Services</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-3">Find Us</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-4">Donations</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-5">Expenses</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-6">Partners</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-7">Blog</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-8">Tools</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Button size='sm'>Donate now</Button>
+      </div>
+    </>
   );
 }
 
